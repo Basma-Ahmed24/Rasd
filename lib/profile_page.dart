54edb8ede9a293565973f9c9006f76590b361c9a
Basma246 <105160104+Basma246@users.noreply.github.com>
2022-12-30 20:48:08 +0200

    b

---
+++ lib/profile_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Profile extends  StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+   return Scaffold();
+  }
+
+}
